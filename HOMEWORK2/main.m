5004ae2b513bf9f65c962f54a2e54f950e7dabdd
Andres <andres97cb@hotmail.com>
2019-11-05 14:28:36 +0100

    añadido prints de funciones necesarias

--- HOMEWORK2/main.m
+++ HOMEWORK2/main.m
@@ -25,12 +25,20 @@ alpha = 1;
 %fprintf("Función descenso del gradiente para num_iter=%f y alfa=%f\n\n",iterations, alpha);
 [theta, J_history] = gradientDescent(X, y, theta, alpha, iterations);
 
+figure();%Crear figura
+
+plot(1:length(J_history), J_history, '-b', 'LineWidth', 2);
+xlabel('Number of iterations');%Título del eje X
+ylabel('Cost J');%Título del eje Y
 plotData(X(:,2:3),y);
 fprintf('Función coste:\n\n');
 [cost, grad] = costFunction(theta, X, y);
 fprintf('Cost at initial theta (zeros): %f\n', cost);
 fprintf('Gradient at initial theta (zeros): \n');
 fprintf(' %f \n', grad);
+prediccion = predict(theta, X);
+tasaAcierto = mean((prediccion==y))*100;
+fprintf("Tasa acierto: %.2f\n",tasaAcierto);
 % Plot Boundary
 % Only need 2 points to define a line, so choose two endpoints (min,max) of X1 and calculate X2
 plotDecisionBoundary(theta,X,y);
@@ -47,9 +55,6 @@ fprintf('Cost at initial theta (zeros): %f\n', cost);
 fprintf('Gradient at initial theta (zeros): \n');
 fprintf(' %f  ', grad);
 fprintf("\n");
-p = predict(theta, X);
-percent2 = mean((p==y))*100;
-fprintf("Tasa acierto: %.2f\n",percent2);
 
 iterations = 920;
 alpha = 0.0000001;
@@ -57,18 +62,31 @@ lambda = 1 ;
 fprintf("Función descenso del gradiente para num_iter=%f y alfa=%f\n\n",iterations, alpha);
 [theta, J_history] = gradientDescentReg(X, y, theta, alpha, iterations, lambda);
 
-%plotData(X(:,2:3),y);
 fprintf('Función coste:\n\n');
 [cost, grad] = costFunctionReg(theta, X, y, lambda);
 fprintf('Cost at initial theta (zeros): %f\n', cost);
 fprintf('Gradient at initial theta (zeros): \n');
 fprintf(' %f  ', grad);
 fprintf("\n");
-p = predict(theta, X);
-percent2 = mean((p==y))*100;
-fprintf("Tasa acierto: %.2f\n",percent2);
+prediccion = predict(theta, X);
+tasaAcierto = mean((prediccion==y))*100;
+fprintf("Tasa acierto: %.2f\n",tasaAcierto);
 figure();%Crear figura
 
 plot(1:length(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');%Título del eje X
-ylabel('Cost J');%Título del eje Y
+% Plot Boundary
+X_mapped = X;
+X = data(:,1:2);
+plotData(X,y);
+X = X_mapped(:,2:29);
+plotDecisionBoundary(theta, X, y);
+hold on;
+title(sprintf('lambda = %g', lambda))
+
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+
+legend('y = 1', 'y = 0', 'Decision boundary')
+hold off;
