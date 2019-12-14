76ed9fd50825516fe5900642dc7449e8907d640c
Andres <andres97cb@hotmail.com>
2019-12-14 13:42:15 +0100

    Ficheros utiles de epd6

---
+++ EPD6/aima-java-aima3e-v1.9.1/aima-core/src/main/java/aima/core/environment/misioneros/MisionerosGoalTest.java
@@ -0,0 +1,13 @@
+package aima.core.environment.misioneros;
+
+import aima.core.search.framework.problem.GoalTest;
+
+
+public class MisionerosGoalTest implements GoalTest {
+	MisionerosBoard goal = new MisionerosBoard(new int[] { 0, 0, 0 });
+
+	public boolean isGoalState(Object state) {
+		MisionerosBoard board = (MisionerosBoard) state;
+		return board.equals(goal);
+	}
+}
