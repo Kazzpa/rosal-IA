ae444c666ae259cfa385ddc75c6c7844097251d8
Andrés <andres97cb@hotmail.com>
2019-12-27 18:31:08 +0100

    ultimos cambios 🤡

--- HOMEWORK5/SelAttBoard.java
+++ HOMEWORK5/SelAttBoard.java
@@ -33,24 +33,19 @@ import java.util.Arrays;
  */
 public class SelAttBoard {
 
-    public static Action A1 = new DynamicAction("a1");
-    public static Action A2 = new DynamicAction("a2");
-    public static Action A3 = new DynamicAction("a3");
-    public static Action A4 = new DynamicAction("a4");
-    public static Action A5 = new DynamicAction("a5");
-    public static Action A6 = new DynamicAction("a6");
-    public static Action A7 = new DynamicAction("a7");
-    public static Action A8 = new DynamicAction("a8");
-    public static Action A9 = new DynamicAction("a9");
-
     private int[] state;
 
-    private final double[][] correlation = {{0.64491, 0.65459, 0.48636, 0.52182,
-        0.58730, 0.55843, 0.53583, 0.35003}, {0.90688, 0.70558, 0.75180, 0.68680,
-        0.75572, 0.72286, 0.45869}, {0.68308, 0.71967, 0.70961, 0.73595, 0.71945,
-        0.43891}, {0.59960, 0.66505, 0.66672, 0.60335, 0.41763}, {0.58126,
-        0.61610, 0.62888, 0.47910}, {0.67590, 0.57736, 0.33874}, {0.66588, 0.34417},
-    {0.42834}};
+    private final double[][] correlation = {
+        {0.64491, 0.65459, 0.48636, 0.52182, 0.58730, 0.55843, 0.53583, 0.35003},
+        {0.90688, 0.70558, 0.75180, 0.68680, 0.75572, 0.72286, 0.45869},
+        {0.68308, 0.71967, 0.70961, 0.73595, 0.71945, 0.43891,},
+        {0.59960, 0.66505, 0.66672, 0.60335, 0.41763},
+        {0.58126, 0.61610, 0.62888, 0.47910},
+        {0.67590, 0.57736, 0.33874},
+        {0.66588, 0.34417},
+        {0.42834}};
+    private final double[] correlationClass = {0.71600, 0.81790, 0.81893,
+        0.69680, 0.68278, 0.81605, 0.75662, 0.71224, 0.42317};
 
     public SelAttBoard() {
         state = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -65,92 +60,43 @@ public class SelAttBoard {
         this(copyBoard.getState());
     }
 
-    SelAttBoard(int boardSize) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
     public int[] getState() {
         return state;
     }
 
-    public int getA1() {
-        return state[0];
+    public void SelAtribute(int i) {
+        state[i] = 1 - state[i];
     }
 
-    public int getA2() {
-        return state[1];
+    public int getNumberOfAtributesSelected() {
+        int res = 0;
+        for (int i = 0; i < state.length; i++) {
+            if (state[i] == 1) {
+                res++;
+            }
+        }
+        return res;
     }
 
-    public int getA3() {
-        return state[2];
+    public double getCorrelationAtr(int i, int j) {
+        double res = 1;
+
+        if (i != j) {
+            //swap to get minimun of both index
+            if (j < i) {
+                int swap = i;
+                i = j;
+                j = swap;
+            }
+            //gets correlation from correlation matrix
+            res = correlation[i][j];
+        }
+        return res;
+
     }
 
-    public int getA4() {
-        return state[3];
-    }
-
-    public int getA5() {
-        return state[4];
-    }
-
-    public int getA6() {
-        return state[5];
-    }
-
-    public int getA7() {
-        return state[6];
-    }
-
-    public int getA8() {
-        return state[7];
-    }
-
-    public int getA9() {
-        return state[8];
-    }
-
-    public int getFarol() {
-        return state[4];
-    }
-
-    public int getTiempo() {
-        return state[5];
-    }
-
-    public void SelA1() {
-        state[0] = 1 - state[0];
-    }
-
-    public void SelA2() {
-        state[1] = 1 - state[1];
-    }
-
-    public void SelA3() {
-        state[2] = 1 - state[2];
-    }
-
-    public void SelA4() {
-        state[3] = 1 - state[3];
-    }
-
-    public void SelA5() {
-        state[4] = 1 - state[4];
-    }
-
-    public void SelA6() {
-        state[5] = 1 - state[5];
-    }
-
-    public void SelA7() {
-        state[6] = 1 - state[6];
-    }
-
-    public void SelA8() {
-        state[7] = 1 - state[7];
-    }
-
-    public void SelA9() {
-        state[8] = 1 - state[8];
+    public double getCorrelationClass(int i) {
+        return correlationClass[i];
     }
 
     public boolean canMove(Action where) {
--- HOMEWORK5/SelAttGenAlgoUtil.java
+++ HOMEWORK5/SelAttGenAlgoUtil.java
@@ -1,97 +1,106 @@
-/*
- * The MIT License
- *
- * Copyright 2019 kzr.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package aima.core.environment.atributos;
 
 import aima.core.search.framework.problem.GoalTest;
 import aima.core.search.local.FitnessFunction;
 import aima.core.search.local.Individual;
-import aima.core.util.datastructure.XYLocation;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 
 /**
  *
  * @author kzr
  */
 public class SelAttGenAlgoUtil {
-    
-    
-	public static FitnessFunction<Integer> getFitnessFunction() {
-		return new SelAttFitnessFunction();
-	}
-	
-	public static GoalTest getGoalTest() {
-		return new SelAttGoalTest();
-	}
-	
-        //generamos un individuo aleatorio
-	public static Individual<Integer> generateRandomIndividual(int boardSize) {
-		List<Integer> individualRepresentation = new ArrayList<Integer>();
-                int aux;
-		for (int i = 0; i < boardSize; i++) {
-                        aux = 0;
-                        if(Math.random()< 0.5){
-                            aux = 1;
+
+    public static FitnessFunction<Integer> getFitnessFunction() {
+        return new SelAttFitnessFunction();
+    }
+
+    public static GoalTest getGoalTest() {
+        return new SelAttGoalTest();
+    }
+
+    //generamos un individuo aleatorio
+    public static Individual<Integer> generateRandomIndividual(int boardSize) {
+        List<Integer> individualRepresentation = new ArrayList<Integer>();
+        int aux;
+        for (int i = 0; i < boardSize; i++) {
+            aux = 0;
+            if (Math.random() < 0.5) {
+                aux = 1;
+            }
+            individualRepresentation.add(aux);
+        }
+        Individual<Integer> individual = new Individual<Integer>(individualRepresentation);
+        return individual;
+    }
+
+    public static Collection<Integer> getFiniteAlphabetForBoardOfSize(int size) {
+        Collection<Integer> fab = new ArrayList<Integer>();
+
+        for (int i = 0; i < size; i++) {
+            fab.add(i);
+        }
+
+        return fab;
+    }
+    //Algoritmo CFS
+
+    public static class SelAttFitnessFunction implements FitnessFunction<Integer> {
+
+        public double apply(Individual<Integer> individual) {
+            double fitness = 0;
+            //Para poder calcular que tan bueno es nuestra solución
+            // debemos de usar su propia clase Board
+            SelAttBoard board = getBoardForIndividual(individual);
+            //int numAtr = board.getNumberOfAtributesSelected();
+            double corClass = 0;
+            double corAtr = 1;
+            int[] boardState = board.getState();
+            int[] stateAux = new int[boardState.length];
+            int numAtr = 0;
+            //creamos un array auxiliar para operar mas facilmente que contendra los indices
+            for (int i = 0; i < boardState.length; i++) {
+                if (boardState[i] == 1) {
+                    stateAux[numAtr] = i;
+                    numAtr++;
+                }
+            }
+            //calcular acumulacion de correlacion
+            int numCor = 0;
+            if (numAtr > 1) {
+                for (int i = 0; i < stateAux.length; i++) {
+                    corClass += board.getCorrelationClass(stateAux[i]);
+                    if (i < stateAux.length - 1) {
+                        for (int j = i + 1; j < stateAux.length; j++) {
+                            corAtr += board.getCorrelationAtr(stateAux[i], stateAux[j]);
+                            numCor++;
                         }
-			individualRepresentation.add(aux);
-		}
-		Individual<Integer> individual = new Individual<Integer>(individualRepresentation);
-		return individual;
-	}
+                    }
+                }
+            } else {
+                corClass = board.getCorrelationClass(stateAux[0]);
+            }
+            //aplicar la media de las correlaciones
+            corClass = corClass / numAtr;
+            corAtr = corAtr / numCor;
+            //Aplicamos el algoritmo CFS
+            fitness = (numAtr * corClass) / Math.sqrt(numAtr + ((numAtr * (numAtr - 1)) * corAtr));
+            return fitness;
+        }
+    }
 
-	public static Collection<Integer> getFiniteAlphabetForBoardOfSize(int size) {
-		Collection<Integer> fab = new ArrayList<Integer>();
+    //devuelve el board de la clase
+    public static SelAttBoard getBoardForIndividual(Individual<Integer> individual) {
+        int boardSize = individual.length();
+        SelAttBoard board = new SelAttBoard();
+        for (int i = 0; i < boardSize; i++) {
+            if (individual.getRepresentation().get(i) == 1) {
+                board.SelAtribute(i);
+            }
+        }
+        return board;
+    }
 
-		for (int i = 0; i < size; i++) {
-			fab.add(i);
-		}
-
-		return fab;
-	}
-	//Algoritmo CFS
-	public static class SelAttFitnessFunction implements FitnessFunction<Integer> {
-
-		public double apply(Individual<Integer> individual) {
-			double fitness = 0;
-                        //Para poder calcular que tan bueno es nuestra solución
-                        // debemos de usar su propia clase Board
-			SelAttBoard board = getBoardForIndividual(individual);
-			
-
-			return fitness;
-		}
-	}
-
-        //devuelve el board de la clase
-	public static SelAttBoard getBoardForIndividual(Individual<Integer> individual) {
-		int boardSize = individual.length();
-		SelAttBoard board = new SelAttBoard(boardSize);
-		
-
-		return board;
-	}
-    
 }
